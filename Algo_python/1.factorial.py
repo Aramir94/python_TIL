a760d9f93d60893118f9d72e0276eb5742caf94f
Aramir-A1DATAINT <jeonghyunsuk777@gmail.com>
2022-10-03 22:25:05 +0900

    recur python

---
+++ Algo_python/1.factorial.py
@@ -0,0 +1,19 @@
+# factorial
+# 5! => 5*4*3*2*1
+
+def iterative_factorial(n):
+    fact =1 
+    for i in range(2, n+1):
+        fact *= i
+    return fact
+print(iterative_factorial(5))
+
+def recur_factorial(n):
+    if n == 1:
+        return n
+    else:
+        tmp = recur_factorial(n-1)
+        tmp = tmp * n
+    return tmp
+    
+print(recur_factorial(5))
